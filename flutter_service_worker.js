4e1ad0370e283ab0c311a478ee5467086b0c5b57
Krzysztof Lisowski <krzysztoflisowski@MacBook-Pro-Krzysztof.local>
2022-06-18 15:48:01 +0200

    set http(s) protocol by incoming request

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a7aaaffb7cc2b692afcea5361dc2b5f7",
-"index.html": "00b77b29515617d9d882b545d53e6ce4",
-"/": "00b77b29515617d9d882b545d53e6ce4",
-"main.dart.js": "48acd8edb91ff6cb784993bf02a7d871",
+"index.html": "a0a64ebe0b463c04e3ff5284554eb711",
+"/": "a0a64ebe0b463c04e3ff5284554eb711",
+"main.dart.js": "8069e3c8945fac9279b5b6bd5e556a13",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
