ded6ebeb483d994c7b16fc00f7cf025848832aa9
Krzysztof Lisowski <krzysztoflisowski@MacBook-Pro-Krzysztof.local>
2022-07-04 22:07:44 +0200

    zmiana wersji plaginu just-audio na 0.9.26 - usuniecie problemu z dekodowaniem mp3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a7aaaffb7cc2b692afcea5361dc2b5f7",
-"index.html": "945dcd1a53f863c7dd94571873c08342",
-"/": "945dcd1a53f863c7dd94571873c08342",
-"main.dart.js": "fbb175a4c29a613a3f3d898e2217e23a",
+"index.html": "04c41da0ebfa1f922a428a8f3af0c457",
+"/": "04c41da0ebfa1f922a428a8f3af0c457",
+"main.dart.js": "4a0e1bb0766109fe2833293252634ad3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
